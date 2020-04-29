36571f2837ee889a51168c63c163f820daae76e7
Sung whan Kim <62971846+lazaros7sungwhan@users.noreply.github.com>
2020-04-29 10:08:46 +0900

    Add files via upload

---
+++ Data_Tatble_form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using ZedGraph;
+
+namespace Gas_chromatography_data_converter_ver._1
+{
+    public partial class Data_Tatble_form1 : Form
+    {
+        static public DataTable dt_2;
+        static public double mass_ppm = 0;
+
+        public Data_Tatble_form1()
+        {
+            InitializeComponent();
+            datagrid2();
+            Gridview_2.DataSource = dt_2;
+            graph();
+
+        }
+
+        static public void datagrid2()
+        {
+            dt_2 = new DataTable { TableName = "DATA TABLE" };
+            dt_2.Columns.AddRange(new DataColumn[] {new DataColumn{ColumnName="temperature",Caption="온도",DataType=typeof(double)},
+            new DataColumn { ColumnName = "area", Caption = "영역", DataType = typeof(double) },
+            new DataColumn { ColumnName = "mass ppm", Caption = "PPM", DataType = typeof(double) },
+            new DataColumn { ColumnName = "mass ppm/sec", Caption = "초당 발생 PPM", DataType = typeof(double) },
+            new DataColumn { ColumnName = "boundary", Caption = "경계온도", DataType = typeof(double) },
+            new DataColumn { ColumnName = "수소량", Caption = "경계온도", DataType = typeof(double) }});
+
+            int t = 0;
+            mass_ppm = 0;
+            foreach( double temp in Program.temperature)
+            {
+                if (temp < Program.Boundary_Temperature)
+                    mass_ppm += (Program.Area_data[t] * Program.Calibration_Var / Program.sample_weigh);
+                t++; 
+            }
+
+            int i = 0;
+            foreach (double temp in Program.temperature)
+            {
+                if (i<1)
+                dt_2.Rows.Add(temp, Program.Area_data[i], (Program.Area_data[i] * Program.Calibration_Var / Program.sample_weigh), (Program.Area_data[i] * Program.Calibration_Var / Program.sample_weigh) / 300, Program.Boundary_Temperature,mass_ppm );
+                else
+                {
+                 dt_2.Rows.Add(temp, Program.Area_data[i], (Program.Area_data[i] * Program.Calibration_Var / Program.sample_weigh), (Program.Area_data[i] * Program.Calibration_Var / Program.sample_weigh) / 300);
+                }
+                i++;
+            }
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void settingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            parameter_window form2 = new parameter_window();
+            form2.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mass_ppm = 0;
+            datagrid2();
+            Gridview_2.DataSource = dt_2;
+            graph();
+        }
+        private void graph()
+        {
+            GraphPane graphpane;
+            graphpane = zedGraphControl1.GraphPane;
+
+            PointPairList _pointpairlist = new PointPairList();
+            int i = 0;
+            graphpane.XAxis.Title.Text = "temperature (oC)";
+            graphpane.YAxis.Title.Text = "mass PPM/sec";
+            graphpane.XAxis.MajorGrid.IsVisible = true;
+            graphpane.XAxis.MinorGrid.IsVisible = true;
+
+            graphpane.YAxis.MajorGrid.IsVisible = true;
+            graphpane.YAxis.MinorGrid.IsVisible = true;
+
+            foreach (double temp in Program.temperature)
+            {       
+                dt_2.Rows.Add(temp, Program.Area_data[i], 
+                    (Program.Area_data[i] * Program.Calibration_Var / Program.sample_weigh),
+                    (Program.Area_data[i] * Program.Calibration_Var / Program.sample_weigh) / 300);
+
+                PointPair _pointpair = new PointPair(temp, (Program.Area_data[i] * Program.Calibration_Var / Program.sample_weigh) / 300);
+                //(Program.Area_data[i] * Program.Calibration_Var / Program.sample_weigh) / 300
+
+                _pointpairlist.Add(_pointpair);
+                i++;
+            }
+            LineItem _lineitem = graphpane.AddCurve("mass PPM/sec",_pointpairlist,Color.Red,SymbolType.None);
+            zedGraphControl1.AxisChange();      
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook workbook = app.Workbooks.Add(Type.Missing);
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = null;
+
+            worksheet = workbook.Sheets["sheet1"];
+            worksheet = workbook.ActiveSheet;
+            worksheet.Name = "CustomerDetail";
+
+            for (int i = 1; i < Gridview_2.Columns.Count+1; i++)
+            {
+                worksheet.Cells[1,i] = Gridview_2.Columns[i - 1].HeaderText;
+            }
+            for (int i = 0; i < Gridview_2.Rows.Count-1; i++)
+            {
+                for (int j = 0; j < Gridview_2.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = Gridview_2.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+
+            SaveFileDialog save_excel  = new SaveFileDialog();
+            save_excel.FileName = "output";
+            save_excel.DefaultExt = ".xlsx";
+            if (save_excel.ShowDialog()==DialogResult.OK)
+            {
+                workbook.SaveAs(save_excel.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            }
+            app.Quit();
+        }
+    }
+}
---
+++ parameter_window.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Gas_chromatography_data_converter_ver._1
+{
+    public partial class parameter_window : Form
+    {
+        
+
+        public parameter_window()
+        {
+            InitializeComponent();
+            data_grid();
+        }
+        private void data_grid()
+        {
+
+            DataTable dt;
+            dt = new DataTable();
+            {   dt.Columns.Add("Parameter", typeof(string));
+                dt.Columns.Add("current", typeof(string));
+                dt.Columns.Add("value", typeof(string));
+            }
+            dt.Rows.Add("Temperature interval", Program.Temperature_Interval);
+            dt.Rows.Add("temperature_per_hour", Program.Temperature_per_hr);
+            dt.Rows.Add("Calbiration", Program.Calibration_Var);
+            dt.Rows.Add("Boundar Temperature", Program.Boundary_Temperature);
+            dt.Rows.Add("Sample Weight", Program.sample_weigh);
+
+            dataGridView1.DataSource = dt;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows[0].Cells[2].Value.ToString() != "")
+                Program.Temperature_Interval = Convert.ToDouble(dataGridView1.Rows[0].Cells[2].FormattedValue);
+            if (dataGridView1.Rows[1].Cells[2].Value.ToString() != "")
+                Program.Temperature_per_hr = Convert.ToDouble(dataGridView1.Rows[1].Cells[2].FormattedValue);
+            if (dataGridView1.Rows[2].Cells[2].Value.ToString() != "")
+                Program.Calibration_Var = Convert.ToDouble(dataGridView1.Rows[2].Cells[2].FormattedValue);
+            if (dataGridView1.Rows[3].Cells[2].Value.ToString() != "")
+                Program.Boundary_Temperature = Convert.ToDouble(dataGridView1.Rows[3].Cells[2].FormattedValue);
+            if (dataGridView1.Rows[4].Cells[2].Value.ToString() != "")
+                Program.sample_weigh = Convert.ToDouble(dataGridView1.Rows[4].Cells[2].FormattedValue);
+
+            MessageBox.Show(Program.Temperature_Interval+"\n"+  
+                Program.Temperature_per_hr + "\n"+ 
+                Program.Calibration_Var + "\n"+
+                Program.Boundary_Temperature +"\n"
+                +Program.sample_weigh);
+
+            using(StreamWriter fsout = new StreamWriter(Program.set_up_file_path))
+            {
+                fsout.Write("1.Temperature Interval for Table:: "+ Program.Temperature_Interval.ToString() + "\n" +
+                        "2.Temperature per hour:: "+ Program.Temperature_per_hr.ToString() + "\n" +
+                        "3.Calibration Value:: "+ Program.Calibration_Var.ToString() + "\n" +
+                        "4.Boundary Temperature:: "+ Program.Boundary_Temperature.ToString() + "\n" +
+                        "5.folder_browsing :: NV-F0101.D, NV-F0202.D, NV-F0303.D" +
+                        ", NV-F0404.D, NV-F0505.D, NV-F0606.D, NV-F0707.D, NV-F0808.D" +
+                        ", NV-F0909.D, NV-F1010.D\n" +
+                        "6.Data_file_name :: intermediate.txt\n" +
+                        "7.sample_weigh :: "+ Program.sample_weigh.ToString());
+            }
+                        
+            data_grid();
+        }
+    }
+}
